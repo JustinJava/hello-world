6fe5d41623c06379075a2bd23c9fbb56a8a02761
justinqin <justin_qin@foxmail.com>
2022-02-23 11:14:27 +0800

    【修改原因】测试 【修改内容】更新HelloWorld2 【修改人】justinqin 【检查人】justinqin

---
+++ src/com/justin/helloworld/HelloWorld2.java
@@ -0,0 +1,11 @@
+package com.justin.helloworld;
+
+/**
+ * @author justinqin
+ * @date 2022/2/23
+ */
+public class HelloWorld2 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
