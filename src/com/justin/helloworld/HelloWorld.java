5c1cfc72a9e9b4f35a4dbff573d00abbf34232a9
justinqin <justin_qin@foxmail.com>
2022-02-23 10:48:43 +0800

    【修改原因】测试 【修改内容】添加Hello World测试内容 【修改人】justinqin 【检查人】justinqin

---
+++ src/com/justin/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.justin.helloworld;
+
+/**
+ * @author kingdee
+ * @date 2022/2/23
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
